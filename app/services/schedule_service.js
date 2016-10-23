6cb92ac6b199799e7e163ff720b3d52536fe317b
gdecuzzi <giseladecuzzi@gmail.com>
2016-10-23 19:12:51 -0300

    deleted unused service

--- app/services/schedule_service.js
+++ app/services/schedule_service.js
@@ -18,28 +18,8 @@ angular.module('ejemploconf.services')
       return _.filter(cache.schedule, function(talk){ return day.isSame(talk.date,'day');});
     }
 
-  function getTalkById(talkId, successCallback, errorCallback){
-    var cachedTalk = _.find(cache.schedule, function(talk){
-      return talk.id == talkId;
-    });
-    if(cachedTalk){
-      successCallback(cachedTalk);
-      return
-    }
-
-    var url = AppSettings.api_url + '/schedule/' + talkId;
-    var canceler = $q.defer();
-    $http.get(url, {timeout: canceler.promise}).then(function(response){
-      var event = response.data;
-      successCallback(event);
-    }, errorCallback);
-    return canceler;
-  }
-
-
   return {
     schedule: getSchedule,
-    scheduledOn: getTalksOnDate,
-    talkById: getTalkById
+    scheduledOn: getTalksOnDate
   }
 });
