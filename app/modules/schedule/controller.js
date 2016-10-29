eb114bfae4b3c245a6fa99eb1263b002af09ac04
gdecuzzi <giseladecuzzi@gmail.com>
2016-10-28 23:43:54 -0300

    3 buttons to filter by dates on schedule view

--- app/modules/schedule/controller.js
+++ app/modules/schedule/controller.js
@@ -2,13 +2,24 @@ angular.module('ejemploconf.controllers')
 .controller('ScheduleCtrl', function($scope, ScheduleService, $state, TalkService) {
     var onScheduleLoaded = function (data){
       $scope.schedule = data;
-      //$scope.schedule = ScheduleService.scheduledOn(moment("2016-10-29"));
     };
-
     ScheduleService.schedule(onScheduleLoaded);
 
-  $scope.setTalk=function(talk){
-    TalkService.selectedTalk=talk;
-    $state.go('talkDetails', {"talkId": talk.id});
-  };
+
+    $scope.yesterdayTalks = function(){
+      $scope.schedule = ScheduleService.scheduledOn(moment("2016-10-28"));
+    };
+
+    $scope.todayTalks = function(){
+      $scope.schedule = ScheduleService.scheduledOn(moment("2016-10-29"));
+    };
+
+    $scope.allTalks = function(){
+      ScheduleService.schedule(onScheduleLoaded);
+    };
+
+    $scope.setTalk=function(talk){
+      TalkService.selectedTalk=talk;
+      $state.go('talkDetails', {"talkId": talk.id});
+    };
 });
